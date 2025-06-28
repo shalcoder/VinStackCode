758636eac0080a2bfd2db132b4846ee54425f819
Vishal M <171483291+shalcoder@users.noreply.github.com>
2025-06-28 21:07:47 +0530

    Updated babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,28 @@
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'nativewind/babel',
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@hooks': './src/hooks',
+            '@services': './src/services',
+            '@store': './src/store',
+            '@utils': './src/utils',
+            '@lib': './src/lib',
+            '@types': './src/types',
+            '@assets': './assets'
+          }
+        }
+      ],
+      'react-native-reanimated/plugin'
+    ]
+  };
+};
